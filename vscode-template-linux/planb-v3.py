d1a470938b3dc0cdc782b2a6dea30c7e2a7bc570
Christopher Rumpf <crumpf@sdsu.edu>
2020-11-25 21:47:11 -0800

    tf 2.4.0-rc3 private model infra up

---
+++ vscode-template-linux/planb-v3.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import datetime
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras import layers
+from tensorflow.keras import activations
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
